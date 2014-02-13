dc13878613734ca489489264de0908f178ee0799
iNont <inont@hotmail.co.th>
2014-02-13 20:04:50 +0700

    Add template

---
+++ src/GameLayer.js
@@ -0,0 +1,18 @@
+var GameLayer = cc.LayerColor.extend({
+    init: function() {
+        this._super( new cc.Color4B( 127, 127, 127, 255 ) );
+        this.setPosition( new cc.Point( 0, 0 ) );
+
+        return true;
+    }
+});
+
+var StartScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new GameLayer();
+        layer.init();
+        this.addChild( layer );
+    }
+});
+
