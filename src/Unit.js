2182f69976c668288bc3f289fbeb908887d35e18
iNont <inont@hotmail.co.th>
2014-03-06 08:37:31 +0700

    add Unit.js

---
+++ src/Unit.js
@@ -0,0 +1,9 @@
+var Unit = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile( 'images/Unit.png' );
+    },
+    update: function( dt ) 
+    {
+    }
+});
