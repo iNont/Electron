bc4242ba10bb2b4f2fe961a872b326686e85ba67
iNont <inont@hotmail.co.th>
2014-03-28 18:44:49 +0700

    cleaned the code MainMenuButton

--- src/MainMenuButton.js
+++ src/MainMenuButton.js
@@ -2,40 +2,50 @@ var MainMenuButton = cc.Sprite.extend({
     ctor: function( layer,number ) {
         this.layer=layer;
         this._super();
-        this.initWithFile( 'images/intro/button'+number+'.png' );
-        this.gab=160*gameScale;
-        this.setScale(gameScale*0.75);
-        this.setOpacity(255/2);
-        this.setPosition(new cc.Point(screenWidth+1350,screenHeight/2-number*this.gab));
+        this.initWithFile( "images/intro/button"+number+".png" );
+        this.setScale( gameScale*MainMenuButton.SCALE_UNSELECTED );
+        this.setOpacity( MainMenuButton.OPACITY_UNSELECTED );
+        this.setPosition(new cc.Point(screenWidth+MainMenuButton.POSITION_INIT,screenHeight/2-number*MainMenuButton.GAP*gameScale));
         this.scheduleUpdate();
         this.selected=false;
     },
     update: function( dt ) {
-        var pos = this.getPosition();
-        if(pos.x>screenWidth)
-            this.setPosition(new cc.Point(pos.x-15,pos.y));
+        var pos=this.getPosition();
+        if( pos.x>screenWidth )
+            this.setPosition( new cc.Point( pos.x-MainMenuButton.SHOW_SPEED,pos.y ) );
     },
     updateHide: function() {
-        var pos = this.getPosition();
-        this.setOpacity(this.getOpacity()-5);
-        if(pos.x<screenWidth+500)
-            this.setPosition(new cc.Point(pos.x+15,pos.y));
+        var pos=this.getPosition();
+        this.setOpacity( this.getOpacity()-MainMenuButton.HIDE_SPEED_OPACITY );
+        if( pos.x<screenWidth+MainMenuButton.POSITION_HIDE )
+            this.setPosition( new cc.Point( pos.x+MainMenuButton.HIDE_SPEED,pos.y ) );
         else
-            this.layer.removeChild(this);
+            this.layer.removeChild( this );
 
     },
     hideThis: function() {
         this.unscheduleUpdate();
-        this.schedule(this.updateHide,0,Infinity,0);
+        this.schedule( this.updateHide,0,Infinity,0 );
     },
     select: function() {
         this.selected=true;
-        this.setScale(gameScale);
-        this.setOpacity(200);
+        this.setScale( gameScale*MainMenuButton.SCALE_SELECTED );
+        this.setOpacity( MainMenuButton.OPACITY_SELECTED );
     },
     unselect: function() {
         this.selected=false;
-        this.setScale(gameScale*0.75);
-        this.setOpacity(255/2);
+        this.setScale( gameScale*MainMenuButton.SCALE_UNSELECTED );
+        this.setOpacity( MainMenuButton.OPACITY_UNSELECTED );
     }
 });
+
+MainMenuButton.GAP = 160;
+MainMenuButton.SCALE_SELECTED = 1;
+MainMenuButton.SCALE_UNSELECTED = 0.75;
+MainMenuButton.OPACITY_SELECTED = 200;
+MainMenuButton.OPACITY_UNSELECTED = 255/2;
+MainMenuButton.POSITION_INIT = 1350;
+MainMenuButton.POSITION_HIDE = 500;
+MainMenuButton.HIDE_SPEED_OPACITY = 5;
+MainMenuButton.SHOW_SPEED = 15;
+MainMenuButton.HIDE_SPEED = 15;
