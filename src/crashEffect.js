aab77dd86e2170277fa020d0495afd8164d2a379
iNont <inont@hotmail.co.th>
2014-03-28 18:53:46 +0700

    cleaned the code CrashEffect

--- src/crashEffect.js
+++ src/crashEffect.js
@@ -3,23 +3,25 @@ var CrashEffect = cc.Sprite.extend({
     	this.layer = layer;
         this._super();
         this.scheduleUpdate();
-        this.setScale(gameScale);
-        this.setPosition(new cc.Point( screenWidth/2, screenHeight/2));
+        this.setScale( gameScale );
+        this.setPosition( new cc.Point( screenWidth/2, screenHeight/2 ) );
     },
     update: function( dt ) {
-    	this.setOpacity(this.getOpacity()+this.diffOpac);
-    	if(this.getOpacity()==255)
-    		this.diffOpac=-17;
-        if(this.getOpacity() === 0){
+    	this.setOpacity( this.getOpacity()+this.diffOpac );
+    	if( this.getOpacity()==255 )
+    		this.diffOpac=-CrashEffect.OPACITY_HIDE;
+        if( this.getOpacity()==0 )
             this.unscheduleUpdate();
-        }
     },
 
-    reset: function(type){
-        var src = "images/"+type+"Effect.png";
+    reset: function( type ){
+        var src="images/"+type+"Effect.png";
         this.initWithFile( src );
-        this.diffOpac = 51;
-        this.setOpacity(0);
+        this.diffOpac=CrashEffect.OPACITY_SHOW;
+        this.setOpacity( 0 );
         this.scheduleUpdate();
     }
 });
+
+CrashEffect.OPACITY_SHOW = 51;
+CrashEffect.OPACITY_HIDE = 17;
