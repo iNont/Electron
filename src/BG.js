fcf95003deb15fb5459eee0f89e09d2d7371ed74
iNont <inont@hotmail.co.th>
2014-03-28 18:24:01 +0700

    cleaned the code BG 'wink animation'

--- src/BG.js
+++ src/BG.js
@@ -7,7 +7,7 @@ var BG = cc.Sprite.extend({
         this.setScale( gameScale );
         this.setPosition( new cc.Point( screenWidth,screenHeight/2 ) );
 		this.setAnchorPoint( new cc.Point( 1,1 ) );
-        this.vx=5*gameScale;
+        this.vx=BG.VELOCITY_X*gameScale;
         this.vy=0;
         this.winkSpeed=0.5;
         this.opaDif=0;
@@ -27,16 +27,21 @@ var BG = cc.Sprite.extend({
     	this.setPosition(new cc.Point(pos.x+this.vx,pos.y+this.vy))
     },
     updateWink: function() {
-        var opacity = this.getOpacity();
+        var opacity=this.getOpacity();
         this.setOpacity( opacity-this.winkSpeed );
-        opacity = this.getOpacity();
-        if( opacity < 85 || opacity > 155) {
+        opacity=this.getOpacity();
+        if( opacity<BG.WINK_OPACITY_MIN || opacity>BG.WINK_OPACITY_MAX )
             this.winkSpeed *= -1;
-        }
     },
 
     startGameAnimation: function() {
-    	this.opaDif=2.5;
-        this.vy=15*gameScale;
+    	this.opaDif=BG.WINK_SPEED;
+        this.vy=BG.VELOCITY_Y*gameScale;
     }
 });
+
+BG.WINK_OPACITY_MIN = 85;
+BG.WINK_OPACITY_MAX = 155;
+BG.WINK_SPEED = 2.5;
+BG.VELOCITY_X = 5;
+BG.VELOCITY_Y = 15;
