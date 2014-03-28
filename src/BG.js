031c275aae2df6ac26abd063b0ffa1b83211d0bc
iNont <inont@hotmail.co.th>
2014-03-28 18:31:08 +0700

    cleaned the code BG 'update & animation'

--- src/BG.js
+++ src/BG.js
@@ -9,39 +9,40 @@ var BG = cc.Sprite.extend({
 		this.setAnchorPoint( new cc.Point( 1,1 ) );
         this.vx=BG.VELOCITY_X*gameScale;
         this.vy=0;
-        this.winkSpeed=0.5;
+        this.winkSpeed=BG.WINK_SPEED;
         this.opaDif=0;
         this.scheduleUpdate();
     },
     update: function( dt ) {
-    	var pos = this.getPosition();
-    	if(pos.x>=this.oneRoundX+screenWidth)
-    		this.setPosition(new cc.Point(screenWidth, pos.y ))
-    	if(pos.y>screenHeight) {
-    		this.setPosition(new cc.Point(pos.x, screenHeight))
+    	var pos=this.getPosition();
+    	if( pos.x>=this.oneRoundX+screenWidth )
+    		this.setPosition( new cc.Point( screenWidth,pos.y ) );
+    	if( pos.y>screenHeight ) {
+    		this.setPosition( new cc.Point( pos.x,screenHeight ) );
     		this.vy=0;
     		this.opaDif=0;
-        	this.schedule(this.updateWink,0,Infinity,0);
+        	this.schedule( this.updateWink,0,Infinity,0 );
     	}
-    	this.setOpacity(this.getOpacity()-this.opaDif);
-    	this.setPosition(new cc.Point(pos.x+this.vx,pos.y+this.vy))
+    	this.setOpacity( this.getOpacity()-this.opaDif );
+    	this.setPosition( new cc.Point( pos.x+this.vx,pos.y+this.vy ) )
     },
     updateWink: function() {
         var opacity=this.getOpacity();
         this.setOpacity( opacity-this.winkSpeed );
         opacity=this.getOpacity();
         if( opacity<BG.WINK_OPACITY_MIN || opacity>BG.WINK_OPACITY_MAX )
-            this.winkSpeed *= -1;
+            this.winkSpeed*=-1;
     },
 
     startGameAnimation: function() {
-    	this.opaDif=BG.WINK_SPEED;
+    	this.opaDif=BG.DIFF_SPEED;
         this.vy=BG.VELOCITY_Y*gameScale;
     }
 });
 
 BG.WINK_OPACITY_MIN = 85;
 BG.WINK_OPACITY_MAX = 155;
-BG.WINK_SPEED = 2.5;
+BG.WINK_SPEED = 0.5;
+BG.DIFF_SPEED = 2.5;
 BG.VELOCITY_X = 5;
 BG.VELOCITY_Y = 15;
