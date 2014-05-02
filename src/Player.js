c0777d240fec26a292a98551b363d5e4e49974ff
iNont <inont@hotmail.co.th>
2014-05-02 13:57:47 +0700

    Cleaned code Player

--- src/Player.js
+++ src/Player.js
@@ -3,14 +3,14 @@ var Player = cc.Sprite.extend({
         this._super();
         this.initWithFile( "images/Player.png" );
         this.scheduleUpdate();
-        this.winkSpeed=Player.WINK_SPEED;
+        this.winkSpeed = Player.WINK_SPEED;
     },
     update: function( dt ) {
-        var opacity=this.getOpacity();
+        var opacity = this.getOpacity();
         this.setOpacity( opacity-this.winkSpeed );
-        opacity=this.getOpacity();
-        if( opacity<Player.WINK_OPACITY_MIN || opacity>Player.WINK_OPACITY_MAX )
-            this.winkSpeed*=-1;
+        opacity = this.getOpacity();
+        if( ( opacity < Player.WINK_OPACITY_MIN ) || ( opacity>Player.WINK_OPACITY_MAX ) )
+            this.winkSpeed *= -1;
     }
 });
 
